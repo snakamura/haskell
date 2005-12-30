2cbf7df1d4a09a649113eaf7fa55ecd2d17f5b90
Satoshi Nakamura <snak@snak.org>
2005-12-30 03:21:43 +0000

    Add initial implementation.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@4 f3cf0176-1a09-0410-9a4c-829831ba871c

---
+++ wiki/Main.hs
@@ -0,0 +1,291 @@
+module Main
+    where
+
+import Char
+import Directory
+import IO
+import List
+import Monad
+import System
+import System.Environment
+import System.Time
+import Text.Regex
+
+dataDir, cgi, url, defaultPage, charset :: String
+dataDir = "/home/snakamura/haskell/wiki/data/"
+cgi = "wiki.cgi"
+url = "http://home.snak.org/~snakamura/" ++ cgi
+defaultPage = "FrontPage"
+charset = "euc-jp"
+
+main :: IO ()
+main = do query <- getQuery
+          content <- hGetContents stdin
+          process $ parseParams query ++ parseParams content
+    where
+        getQuery :: IO String
+        getQuery = catch (getEnv "QUERY_STRING")
+                         (\ e -> if isDoesNotExistError e
+                                    then return ""
+                                    else ioError e)
+
+process :: Params -> IO ()
+process params = do
+    case getMode $ getParam params "mode" of
+         VIEW   -> do b <- existData page
+                      if b then printViewHtml page
+                           else printEditHtml page
+         EDIT   -> printEditHtml page
+         UPDATE -> do if body /= "" then do updateData page body
+                                            printUpdateHtml page
+                                    else do removeData page
+                                            printUpdateHtml defaultPage
+         LIST -> printListHtml
+    where
+        page = if page' == "" then defaultPage else page'
+        page' = getParam params "page"
+        body = getParam params "body"
+
+printViewHtml :: String -> IO ()
+printViewHtml page = do
+    body <- getData page
+    formattedBody <- formatBody body
+    printContentType
+    printLine ""
+    printLine "<html>"
+    printLine "<head>"
+    printLine $ "<title>" ++ escapeHtml page ++ "</title>"
+    printLine "</head>"
+    printLine "<body>"
+    printNavigator page
+    printLine $ "<h1>" ++ escapeHtml page ++ "</h1>"
+    printLine formattedBody
+    printLine "</body>"
+    printLine "</html>"
+
+printEditHtml :: String -> IO ()
+printEditHtml page = do
+    body <- getData page
+    printContentType
+    printLine ""
+    printLine "<html>"
+    printLine "<head>"
+    printLine $ "<title>" ++ escapeHtml page ++ "</title>"
+    printLine "</head>"
+    printLine "<body>"
+    printNavigator page
+    printLine $ "<h1>" ++ escapeHtml page ++ "</h1>"
+    printLine $ "<form action=\""++ cgi ++ "\" method=\"POST\">"
+    putStr "<div><textarea name=\"body\" rows=\"20\" cols=\"80\">"
+    putStr $ escapeHtml body
+    printLine "</textarea></div>"
+    printLine "<div><input type=\"submit\" value=\"Save\"></input></div>"
+    printLine $ "<input name=\"page\" type=\"hidden\" value=\"" ++ escapeHtml page ++ "\"></input>"
+    printLine "<input name=\"mode\" type=\"hidden\" value=\"update\"></input>"
+    printLine "</form>"
+    printLine "</body>"
+    printLine "</html>"
+
+printUpdateHtml :: String -> IO ()
+printUpdateHtml page = do
+    printContentType
+    printLine $ "Location: " ++ thisURL
+    printLine ""
+    printLine "<html>"
+    printLine "<head>"
+    printLine $ "<meta http-equiv=\"refresh\" content=\"0;url=" ++ thisURL ++ "\">"
+    printLine $ "<title>" ++ escapeHtml page ++ "</title>"
+    printLine "</head>"
+    printLine "<body>"
+    printLine $ "<p><a href=\"" ++ cgi ++ "?page=" ++ encodeURLComponent page ++ "\">Click here</a></p>"
+    printLine "</body>"
+    printLine "</html>"
+    where
+        thisURL = url ++ "?page=" ++ encodeURLComponent page
+
+printListHtml :: IO ()
+printListHtml = do
+    list <- listData
+    printContentType
+    printLine ""
+    printLine "<html>"
+    printLine "<head>"
+    printLine $ "<title>List</title>"
+    printLine "</head>"
+    printLine "<body>"
+    printNavigator ""
+    printLine $ "<h1>List</h1>"
+    printLine "<ul>"
+    mapM_ printIndex list
+    printLine "</ul>"
+    printLine "</body>"
+    printLine "</html>"
+    where
+        printIndex :: DataMetadata -> IO ()
+        printIndex (DM s t) = do c <- toCalendarTime t
+                                 printLine $ "<li>" ++ formatDate c ++ " : " ++ formatLink s s ++ "</li>"
+        formatDate :: CalendarTime -> String
+        formatDate c = (show $ ctYear c) ++ "/" ++
+                       (show $ (fromEnum $ ctMonth c) + 1) ++ "/" ++
+                       (show $ ctDay c) ++ " " ++
+                       (show $ ctHour c) ++ ":" ++
+                       (show $ ctMin c) ++ ":" ++
+                       (show $ ctSec c)
+
+printContentType :: IO ()
+printContentType = printLine $ "Content-Type: text/html; charset=" ++ charset
+
+printNavigator :: String -> IO ()
+printNavigator page = do
+    printLine "<div>"
+    if page /= ""
+       then printLine $ "<a href=\"" ++ url ++ "?mode=edit&page=" ++ encodeURLComponent page ++ "\">Edit</a> | "
+       else return ()
+    printLine $ "<a href=\"" ++ url ++ "?page=FrontPage\">Top</a> | "
+    printLine $ "<a href=\"" ++ url ++ "?mode=list\">List</a>"
+    printLine "</div>"
+
+printLine :: String -> IO ()
+printLine s = do putStr s
+                 putStr "\r\n"
+
+formatBody :: String -> IO String
+formatBody body = do x <- foldM (\ l r -> f r >>= return . (l ++))
+                                [] (lines $ normalizeNewLine body)
+                     return $ "<p>" ++ x ++ "</p>"
+    where
+        f :: String -> IO String
+        f ""   = return "</p>\n<p>"
+        f line = do l <- formatInline line
+                    return $ l ++ "\n"
+        normalizeNewLine = filter ('\r' /=)
+
+formatInline :: String -> IO String
+formatInline = formatWikiName
+
+formatWikiName :: String -> IO String
+formatWikiName s = case matchRegexAll regex s of
+                        Just (b, m, a, _) -> do f <- format m
+                                                r <- formatWikiName a
+                                                return $ escapeHtml b ++ f ++ r
+                        Nothing -> return $ escapeHtml s
+    where
+        regex = mkRegex "\\b([A-Z]([A-Za-z])+){2,}\\b"
+        format :: String -> IO String
+        format page = do e <- existData page
+                         if e then return $ formatLink page page
+                              else return $ formatLink page "?" ++ page
+
+formatLink :: String -> String -> String
+formatLink page content = "<a href=\"" ++ url ++ "?page=" ++ page ++ "\">" ++ content ++ "</a>"
+
+data DataMetadata = DM String ClockTime
+
+existData :: String -> IO Bool
+existData = doesFileExist . getDataPath
+
+getData :: String -> IO String
+getData page = catch (readFile $ getDataPath page)
+                     (\ e -> return "")
+
+updateData :: String -> String -> IO ()
+updateData = writeFile . getDataPath
+
+removeData :: String -> IO ()
+removeData = removeFile . getDataPath
+
+listData :: IO [DataMetadata]
+listData = getDirectoryContents dataDir >>=
+           filterM isFile >>=
+           return . sort >>=
+           mapM pairFileTime
+    where
+        isFile :: String -> IO Bool
+        isFile (c:_)  = return $ c /= '.'
+        pairFileTime :: String -> IO DataMetadata
+        pairFileTime file = (getModificationTime $ getDataPath file) >>=
+                            return . DM file
+
+getDataPath :: String -> FilePath
+getDataPath page = dataDir ++ page
+
+
+data Mode = VIEW
+          | EDIT
+          | UPDATE
+          | LIST
+
+getMode :: String -> Mode
+getMode mode | mode == "edit"   = EDIT
+             | mode == "update" = UPDATE
+             | mode == "list"   = LIST
+             | otherwise        = VIEW
+
+
+type Param = (String, String)
+type Params = [Param]
+
+getParam :: Params -> String -> String
+getParam params name = case lookup name params of
+                            Just v  -> v
+                            Nothing -> ""
+{-
+getParam [] _ = ""
+getParam ((n, v):params) name
+    | n == name = v
+    | otherwise = getParam params name 
+-}
+
+parseParams :: String -> Params
+parseParams = map parseParam . paramList
+    where
+        paramList :: String -> [String]
+        paramList "" = []
+        paramList s = case splitString '&' s of
+                           (_, [])    -> [s]
+                           (xs, ys) -> xs:(paramList ys)
+
+parseParam :: String -> Param
+parseParam = decodeValue . splitString '='
+    where
+        decodeValue :: Param -> Param
+        decodeValue (name, value) = (name, decode value)
+        decode :: String -> String
+        decode "" = ""
+        decode ('%':r@(c1:c2:s))
+            | isHexDigit c1 && isHexDigit c2 = (decodeChar c1 c2):(decode s)
+            | otherwise                      = '%':(decode r)
+        decode (c:s) | c == '+'  = ' ':(decode s)
+                     | otherwise = c:(decode s)
+        decodeChar :: Char -> Char -> Char
+        decodeChar c1 c2 = chr ((digitToInt c1)*16 + digitToInt c2)
+
+
+splitString :: Char -> String -> (String, String)
+splitString c s = case break (c ==) s of
+                       (_, [])    -> (s, [])
+                       (xs, _:ys) -> (xs, ys)
+
+escapeHtml :: String -> String
+--escapeHtml = foldr ((++) . escapeHtmlChar) []
+escapeHtml = concatMap escapeHtmlChar
+    where
+        escapeHtmlChar :: Char -> String
+        escapeHtmlChar c | c == '<'  = "&lt;"
+                         | c == '>'  = "&gt;"
+                         | c == '&'  = "&amp;"
+                         | c == '\"' = "&quot;"
+                         | otherwise = [c]
+
+encodeURLComponent :: String -> String
+encodeURLComponent = concatMap encodeURLComponentChar
+    where
+        encodeURLComponentChar :: Char -> String
+        encodeURLComponentChar c
+            | 'A' <= c && c <= 'Z' = [c]
+            | 'a' <= c && c <= 'z' = [c]
+            | '0' <= c && c <= '9' = [c]
+            | c == '_'             = [c]
+            | otherwise            = '%':(intToDigit $ n `div` 16):(intToDigit $ n `mod` 16):[]
+            where
+                n = ord c
