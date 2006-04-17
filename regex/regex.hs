517effdc20e75b6fa3187bda393863407c6e2731
Satoshi Nakamura <snak@snak.org>
2006-04-17 22:21:22 +0000

    Define types for each piece.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@33 f3cf0176-1a09-0410-9a4c-829831ba871c

--- regex/regex.hs
+++ regex/regex.hs
@@ -1,10 +1,13 @@
 import Char
 
-data Regex = Branch Regex Regex
-           | Seq [Regex]
-           | Atom Char Quantifier
-           | Group Regex Quantifier
-           | Empty
+type Regex = [Seq]
+
+type Seq = [Piece]
+
+type Piece = (Atom, Quantifier)
+
+data Atom = CharAtom Char
+          | Group Regex
     deriving Show
 
 data Quantifier = None
@@ -19,45 +22,44 @@ parse s = let (regex, rest) = parseBranch s
               _  -> error "Invalid pattern."
 
 parseBranch :: String -> (Regex, String)
-parseBranch = uncurry makeBranch . parsePiece
-    where makeBranch regex []      = (regex, [])
-          makeBranch regex ('|':s) = let (regex', rest') = parseBranch s
-                                     in (Branch regex regex', rest')
-          makeBranch regex s       = (regex, s)
+parseBranch = uncurry parseNext . parsePiece
+    where parseNext seq []      = ([seq], [])
+          parseNext seq ('|':s) = uncurry makeBranch $ parseBranch s
+              where
+                  makeBranch regex s = (seq:regex, s)
+          parseNext seq s       = ([seq], s)
 
-parsePiece :: String -> (Regex, String)
+parsePiece :: String -> (Seq, String)
 parsePiece = uncurry parseNext . parseOnePiece
     where
         parseOnePiece s = let (regex, rest) = parseGroup s
                           in case regex of
-                                 Empty -> parseAtom s
-                                 _     -> (regex, rest)
-        parseNext Empty rest         = (Empty, rest)
-        parseNext regex []           = (regex, []  )
-        parseNext regex rest@('|':_) = (regex, rest)
-        parseNext regex rest         = uncurry makeSeq $ parsePiece rest
+                                 Nothing -> parseAtom s
+                                 _       -> (regex, rest)
+        parseNext Nothing rest       = ([],  rest)
+        parseNext (Just p) []        = ([p], []  )
+        parseNext (Just p) rest      = uncurry makeSeq $ parsePiece rest
             where
-                makeSeq Empty    s = (regex,          s)
-                makeSeq (Seq rs) s = (Seq (regex:rs), s)
-                makeSeq r        s = (Seq [regex, r], s)
+                makeSeq [] s = ([p],     s)
+                makeSeq ps s = (p:ps,    s)
 
-parseGroup :: String -> (Regex, String)
+parseGroup :: String -> (Maybe Piece, String)
 parseGroup ('(':s) = uncurry makeGroup $ parseBranch s
     where
-        makeGroup regex (')':'?':s) = (Group regex Optional, s)
-        makeGroup regex (')':'*':s) = (Group regex Repeat,   s)
-        makeGroup regex (')':s)     = (Group regex None,     s)
-        makeGroup _ _               = error "Invalid pattern."
-parseGroup s       = (Empty, s)
+        makeGroup regex (')':'?':s) = (Just (Group regex, Optional), s    )
+        makeGroup regex (')':'*':s) = (Just (Group regex, Repeat),   s    )
+        makeGroup regex (')':s)     = (Just (Group regex, None),     s    )
+        makeGroup _ _               = (Nothing,                      '(':s)
+parseGroup s       = (Nothing, s)
 
-parseAtom :: String -> (Regex, String)
-parseAtom []                      = (Empty, [])
+parseAtom :: String -> (Maybe Piece, String)
+parseAtom []                      = (Nothing, [])
 parseAtom cs@(c:s) | isAtomChar c = makeAtom s
-                   | otherwise    = (Empty, cs)
+                   | otherwise    = (Nothing, cs)
     where
-        makeAtom ('?':s) = (Atom c Optional, s)
-        makeAtom ('*':s) = (Atom c Repeat,   s)
-        makeAtom s       = (Atom c None,     s)
+        makeAtom ('?':s) = (Just (CharAtom c, Optional), s)
+        makeAtom ('*':s) = (Just (CharAtom c, Repeat),   s)
+        makeAtom s       = (Just (CharAtom c, None),     s)
 
 isAtomChar :: Char -> Bool
 isAtomChar = flip notElem "|()*?"
