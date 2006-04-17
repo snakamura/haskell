3df210a94a14f173ca0cf121b732782cf5ec24b5
Satoshi Nakamura <snak@snak.org>
2006-04-17 01:12:15 +0000

    Add regex.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@30 f3cf0176-1a09-0410-9a4c-829831ba871c

---
+++ regex/regex.hs
@@ -0,0 +1,60 @@
+import Char
+
+data Regex = Branch Regex Regex
+           | Seq Regex Regex
+           | Atom Char Quantifier
+           | Group Regex Quantifier
+           | Empty
+    deriving Show
+
+data Quantifier = None
+                | Optional
+                | Repeat
+    deriving Show
+
+parse :: String -> Regex
+parse s = let (regex, rest) = parseBranch s
+          in case rest of
+              [] -> regex
+              _  -> error "Invalid pattern."
+
+parseBranch :: String -> (Regex, String)
+parseBranch = uncurry makeBranch . parsePiece
+    where makeBranch regex []      = (regex, [])
+          makeBranch regex ('|':s) = let (regex', rest') = parseBranch s
+                                     in (Branch regex regex', rest')
+          makeBranch regex s       = (regex, s)
+
+parsePiece :: String -> (Regex, String)
+parsePiece []      = (Empty, [])
+parsePiece s       = uncurry parseNext $ case s of
+                                             '(':cs -> parseGroup cs
+                                             _      -> parseAtom s
+    where
+        parseNext Empty rest         = (Empty, rest)
+        parseNext regex []           = (regex, []  )
+        parseNext regex rest@('|':_) = (regex, rest)
+        parseNext regex rest         = let (regex', rest') = parsePiece rest
+                                       in case regex' of
+                                              Empty -> (regex, rest')
+                                              _     -> (Seq regex regex', rest')
+
+parseGroup :: String -> (Regex, String)
+parseGroup = uncurry makeGroup . parseBranch
+    where
+        makeGroup regex (')':'?':s) = (Group regex Optional, s)
+        makeGroup regex (')':'*':s) = (Group regex Repeat,   s)
+        makeGroup regex (')':s)     = (Group regex None,     s)
+        makeGroup _ _               = error "Invalid pattern."
+
+parseAtom :: String -> (Regex, String)
+parseAtom []                      = (Empty, [])
+parseAtom cs@(c:s) | isAtomChar c = makeAtom s
+                   | otherwise    = (Empty, cs)
+    where
+        makeAtom ('?':s) = (Atom c Optional, s)
+        makeAtom ('*':s) = (Atom c Repeat,   s)
+        makeAtom s       = (Atom c None,     s)
+
+isAtomChar :: Char -> Bool
+isAtomChar = flip notElem "|()*?"
