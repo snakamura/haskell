0368449dbf7f89fd63396e1bea254a9a4bed480c
Satoshi Nakamura <snak@snak.org>
2006-04-18 11:02:18 +0000

    Use newtype instead of type.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@38 f3cf0176-1a09-0410-9a4c-829831ba871c

--- regex/regex.hs
+++ regex/regex.hs
@@ -16,92 +16,98 @@ data Quantifier = None
     deriving Show
 
 parse :: String -> Maybe Regex
-parse = fst . parser
+parse = fst . runParser parser
     where
-        parser = parseBranch |>>= \regex ->
+        parser = branch |>>= \regex ->
                  empty |>>
                  always regex
 
-parseBranch :: Parser String Regex
-parseBranch =     empty |>>
-                  always [[]]
-              <|> parsePiece |>>= \ps ->
-                  char '|' |>>
-                  parseBranch |>>= \regex ->
-                  always (ps:regex)
-              <|> parsePiece |>>= \ps ->
-                  always [ps]
+branch :: Parser String Regex
+branch =     (empty |>>
+              always [[]])
+         <|> (piece |>>= \ps ->
+              char '|' |>>
+              branch |>>= \regex ->
+              always (ps:regex))
+         <|> (piece |>>= \ps ->
+              always [ps])
 
-parsePiece :: Parser String Seq
-parsePiece =     parseGroup |>>= \g ->
-                 parsePiece |>>= \ps ->
-                 always (g:ps)
-             <|> parseAtom |>>= \a ->
-                 parsePiece |>>= \ps ->
-                 always (a:ps)
-             <|> always []
+piece :: Parser String Seq
+piece =     (group |>>= \g ->
+             piece |>>= \ps ->
+             always (g:ps))
+        <|> (atom |>>= \a ->
+             piece |>>= \ps ->
+             always (a:ps))
+        <|>  always []
 
-parseGroup :: Parser String Piece
-parseGroup =     char '(' |>>
-                 parseBranch |>>= \regex ->
-                 char ')' |>>
-                 char '?' |>>
-                 always (Group regex, Optional)
-             <|> char '(' |>>
-                 parseBranch |>>= \regex ->
-                 char ')' |>>
-                 char '*' |>>
-                 always (Group regex, Repeat)
-             <|> char '(' |>>
-                 parseBranch |>>= \regex ->
-                 char ')' |>>
-                 always (Group regex, None)
+group :: Parser String Piece
+group =     (char '(' |>>
+             branch |>>= \regex ->
+             char ')' |>>
+             char '?' |>>
+             always (Group regex, Optional))
+        <|> (char '(' |>>
+             branch |>>= \regex ->
+             char ')' |>>
+             char '*' |>>
+             always (Group regex, Repeat))
+        <|> (char '(' |>>
+             branch |>>= \regex ->
+             char ')' |>>
+             always (Group regex, None))
 
-parseAtom :: Parser String Piece
-parseAtom =     charOf isAtomChar |>>= \c ->
-                char '?' |>>
-                always (CharAtom c, Optional)
-            <|> charOf isAtomChar |>>= \c ->
-                char '*' |>>
-                always (CharAtom c, Repeat  )
-            <|> charOf isAtomChar |>>= \c ->
-                always (CharAtom c, None    )
+atom :: Parser String Piece
+atom =     (charOf isAtomChar |>>= \c ->
+            char '?' |>>
+            always (CharAtom c, Optional))
+       <|> (charOf isAtomChar |>>= \c ->
+            char '*' |>>
+            always (CharAtom c, Repeat  ))
+       <|> (charOf isAtomChar |>>= \c ->
+            always (CharAtom c, None    ))
 
 empty :: Parser String ()
-empty [] = (Just (), [])
-empty s  = (Nothing, s )
+empty = Parser empty'
+    where
+        empty' [] = (Just (), [])
+        empty' s  = (Nothing, s )
 
 always :: b -> Parser a b
-always x s = (Just x, s)
+always = Parser . always'
+    where
+        always' x s = (Just x, s)
 
 char :: Char -> Parser String Char
 char c = charOf (==c)
 
 charOf :: (Char -> Bool) -> Parser String Char
-charOf f []                = (Nothing, []   )
-charOf f (c:s) | f c       = (Just c,  s    )
-               | otherwise = (Nothing, (c:s))
+charOf = Parser . charOf'
+    where
+        charOf' f []                = (Nothing, []   )
+        charOf' f (c:s) | f c       = (Just c,  s    )
+                        | otherwise = (Nothing, (c:s))
 
 isAtomChar :: Char -> Bool
 isAtomChar = flip notElem "|()*?"
 
 
-type Parser a b = a -> (Maybe b, a)
+newtype Parser a b = Parser { runParser :: a -> (Maybe b, a) }
 
-infixl 1 <|>
+infixr 1 <|>
 (<|>) :: Parser a b -> Parser a b -> Parser a b
-(<|>) parse1 parse2 = \s -> let (x, r) = parse1 s
-                            in case x of
-                                   Nothing -> parse2 s
-                                   _       -> (x, r)
+(<|>) (Parser parse1) (Parser parse2) = Parser $ \s -> let (x, r) = parse1 s
+                                                       in case x of
+                                                              Nothing -> parse2 s
+                                                              _       -> (x, r)
 
-infixl 7 |>>=
+infixl 9 |>>=
 (|>>=) :: Parser a b -> (b -> Parser a c) -> Parser a c
-(|>>=) parser1 f = \s -> let (x, r) = parser1 s
-                         in case x of
-                                Nothing -> (Nothing, r)
-                                Just v  -> f v r
+(|>>=) (Parser parser1) f = Parser $ \s -> let (x, r) = parser1 s
+                                           in case x of
+                                                  Nothing -> (Nothing, r)
+                                                  Just v  -> runParser (f v) $ r
 
-infixl 7 |>>
+infixl 9 |>>
 (|>>) :: Parser a b -> Parser a c -> Parser a c
 (|>>) parser1 parser2 = parser1 |>>= \_ -> parser2
