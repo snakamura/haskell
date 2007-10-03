3a507aee5c39e40f228809f56708aca7618ca522
Satoshi Nakamura <snak@snak.org>
2007-10-03 06:49:46 +0000

    Fixed column doesn't work correctly.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@71 f3cf0176-1a09-0410-9a4c-829831ba871c

--- nakagawa/column.hs
+++ nakagawa/column.hs
@@ -1,4 +1,4 @@
-column n = reverse $ takeWhile (/=' ') $ map c [0..]
+column n = reverse $ takeWhile (/= ' ') $ map c [0..]
  where
      c 0 = cycle ['A'..'Z'] !! n
-     c m = cycle (concatMap (replicate (26^m)) $ ' ':['A'..'Z']) !! n
+     c m = (replicate (sum $ map (26^) [1..m]) ' ' ++ cycle (concatMap (replicate (26^m)) ['A'..'Z'])) !! n
