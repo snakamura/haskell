f0373a8d2e8f98c2be9a0a6b43c04254b8d5f8d1
Satoshi Nakamura <snak@snak.org>
2007-10-04 04:06:28 +0000

    Add code to generate edges.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@74 f3cf0176-1a09-0410-9a4c-829831ba871c

--- nakagawa/graph.hs
+++ nakagawa/graph.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+import Control.Monad (liftM)
+import Data.List (sort)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+main = do
+    words <- liftM lines getContents
+    print $ count $ genEdges words
+
 --count :: Ord a => [(a, a)] -> Int
-count = Set.size . mergeEdges
+count = Set.size . merge{-Edges-}
 
 --mergeEdges :: Ord a => [(a, a)] -> Set (Set a)
 mergeEdges = merge . Set.fromList . map (\ (x, y) -> Set.fromList [x, y])
@@ -17,3 +23,23 @@ merge = Set.fold merge' Set.empty
                                in if not $ Set.null $ Set.intersection h th
                                       then merge' (Set.union h th) tt
                                       else Set.insert th $ merge' h tt
+
+hasLink x y = hasLink' (sort x) (sort y)
+ where
+     hasLink' xss@(x:xs) yss@(y:ys) | x == y    = hasLink' xs ys
+                                    | otherwise = hasLink'' xs yss ||
+                                                  hasLink'' xs ys ||
+                                                  hasLink'' xss ys
+     hasLink' []         _                      = True
+     hasLink'' (x:xs) (y:ys) | x == y    = hasLink'' xs ys
+                             | otherwise = False
+     hasLink'' _      []                 = True
+     hasLink'' []     _                  = True
+
+genEdges (word:words) =
+    let l = [ [word, w] | w <- words, hasLink word w ]
+        s = if null l
+                then Set.singleton $ Set.singleton word
+                else Set.fromList $ map Set.fromList l
+    in Set.union s $ genEdges words
+genEdges [] = Set.empty
