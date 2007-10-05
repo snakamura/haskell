47a83d3baa03450dd7af7a67785e597dbf3d85ca
Satoshi Nakamura <snak@snak.org>
2007-10-05 03:04:43 +0000

    Improve performance.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@77 f3cf0176-1a09-0410-9a4c-829831ba871c

--- nakagawa/graph.hs
+++ nakagawa/graph.hs
@@ -7,9 +7,7 @@ import qualified Data.Set as Set
 
 main = do
     words <- liftM lines getContents
-    print $ count $ gen words
-
-count = Set.size . merge
+    print $ Set.size $ merge $ gen $ map sort words
 
 merge = Set.fold merge' Set.empty
  where
@@ -19,19 +17,15 @@ merge = Set.fold merge' Set.empty
                                       then merge' (Set.union h th) tt
                                       else Set.insert th $ merge' h tt
 
-hasLink x y = hasLink' (sort x) (sort y)
+hasLink []         _                      = True
+hasLink xss@(x:xs) yss@(y:ys) | x == y    = hasLink xs ys
+                              | otherwise = hasLinkL xs yss || xs == ys || hasLinkR xss ys
  where
-     hasLink' xss@(x:xs) yss@(y:ys) | x == y    = hasLink' xs ys
-                                    | otherwise = hasLinkL xs yss ||
-                                                  xs == ys ||
-                                                  hasLinkR xss ys
-     hasLink' []         _                      = True
      hasLinkL xss@(x:xs) (y:ys) | x == y    = hasLinkL xs ys
                                 | otherwise = xss == ys
      hasLinkL []         [_]                = True
      hasLinkR = flip hasLinkL
 
-gen (word:words) =
-    let s = Set.fromList $ word:[ w | w <- words, hasLink word w ]
-    in Set.insert s $ gen words
+gen (word:words) = let s = Set.fromList $ word:[ w | w <- words, hasLink word w ]
+                   in Set.insert s $ gen words
 gen [] = Set.empty
