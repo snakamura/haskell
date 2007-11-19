32eb4c92b2465426dcae4778906abdd17ea2c9df
Satoshi Nakamura <snak@snak.org>
2007-11-19 05:22:26 +0000

    Update mdo.hs.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@81 f3cf0176-1a09-0410-9a4c-829831ba871c

--- mdo/mdo.hs
+++ mdo/mdo.hs
@@ -1,14 +1,46 @@
-import Data.IORef
+{-# LANGUAGE RecursiveDo #-}
+
 import Control.Monad.Fix
+import Data.IORef
 
 data List = Cons {car::Int, cdr::(IORef List)} | Nil
 
-main = mdo
+main = undefined
+
+
+test0 = mdo
   p <- newIORef (Cons 1 p)
   ones <- readIORef p
-  print (car ones)
+  print $ car ones
 
-test = do
-    p <- mfix (\q -> newIORef (Cons 1 q))
+test1 = do
+--    p <- mfix (\q -> newIORef (Cons 1 q))
+    (_, p) <- mfix (\ ~(p, _) -> do
+        p <- newIORef (Cons 1 p);
+        return (p, p))
     ones <- readIORef p
-    print (car ones)
+    print $ car ones
+
+
+test2 = mdo
+    p <- newIORef (Cons 1 q)
+    q <- newIORef (Cons 2 p)
+    x <- readIORef p
+    y <- readIORef q
+    printCarCadr x -- (1,2)
+    printCarCadr y -- (2,1)
+
+
+test3 = do
+    (p, q) <- mfix (\ ~(p, q) -> do
+        p <- newIORef (Cons 1 q)
+        q <- newIORef (Cons 2 p)
+        return (p, q))
+    x <- readIORef p
+    y <- readIORef q
+    printCarCadr x -- (1,2)
+    printCarCadr y -- (2,1)
+
+printCarCadr x = do
+  y <- readIORef (cdr x)
+  print (car x, car y)
