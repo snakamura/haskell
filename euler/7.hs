6c7066eef2a70383373393e5f824f33ab8098292
Satoshi Nakamura <snak@snak.org>
2008-02-27 09:23:58 +0000

    Added monadic versions.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@87 f3cf0176-1a09-0410-9a4c-829831ba871c

--- euler/7.hs
+++ euler/7.hs
@@ -1,3 +1,6 @@
+import Control.Monad.Reader
+import Control.Monad.State
+
 value = last $ take 10001 primes
 
 primes = primes' [] $ 2:[3,5..]
@@ -7,5 +10,33 @@ primes' found (c:candidates) | isPrime found c = c:primes' (found ++ [c]) candid
                              | otherwise       = primes' found candidates
 
 isPrime _      1 = False
-isPrime primes n = let m = ceiling $ sqrt $ fromInteger n
+isPrime primes n = let m = ceiling $ sqrt $ fromIntegral n
                    in and $ map (\p -> n `mod` p /= 0) $ takeWhile (<= m) primes
+
+
+primesS = evalState (primesS' $ 2:[3,5..]) []
+
+primesS' [] = return []
+primesS' (c:candidates) = do
+    b <- isPrimeS c
+    if b
+        then do
+            modify (++ [c])
+            liftM (c:) $ primesS' candidates
+        else primesS' candidates
+
+isPrimeS n = do
+    primes <- get
+    return $ isPrime primes n
+
+
+primesR = runReader (primesR' $ 2:[3,5..]) []
+
+primesR' [] = return []
+primesR' (c:candidates) = do
+    b <- isPrimeR c
+    if b
+        then local (++ [c]) $ liftM (c:) $ primesR' candidates
+        else primesR' candidates
+
+isPrimeR n = ask >>= return . flip isPrime n
