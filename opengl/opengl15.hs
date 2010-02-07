3328c90fc6dd64edf3f05a6d1a64efd6375f7490
Satoshi Nakamura <snak@snak.org>
2010-02-07 23:10:10 +0000

    Use a VBO.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@154 f3cf0176-1a09-0410-9a4c-829831ba871c

---
+++ opengl/opengl15.hs
@@ -0,0 +1,66 @@
+import Control.Monad
+import Data.Array.Storable
+import Foreign.Ptr
+import Foreign.Storable
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+
+main =
+  do (progName, _) <- getArgsAndInitialize
+     initialDisplayMode $= [RGBAMode]
+     window <- createWindow "Window"
+     clearColor $= Color4 1.0 1.0 1.0 0.0
+     program <- join $ liftM2 createProgram (createShader vertexShaderSource) 
+                                            (createShader fragmentShaderSource)
+     displayCallback $= display program
+     mainLoop
+
+display program =
+  do clear [ColorBuffer]
+     currentProgram $= Just program
+     buffer <- createBuffer ([ 0.0,  0.9, 1.0, 0.0, 0.0,
+                              -0.9, -0.9, 0.0, 1.0, 0.0,
+                               0.9, -0.9, 0.0, 0.0, 1.0] :: [GLfloat])
+     positionLoc <- get $ attribLocation program "a_position"
+     colorLoc <- get $ attribLocation program "a_color"
+     bindBuffer ArrayBuffer $= Just buffer
+     vertexAttribPointer positionLoc $= (ToFloat, VertexArrayDescriptor 2 Float (toEnum (5*sizeOf(0 :: GLfloat))) nullPtr)
+     vertexAttribArray positionLoc $= Enabled
+     vertexAttribPointer colorLoc $= (ToFloat, VertexArrayDescriptor 3 Float (toEnum (5*sizeOf(0 :: GLfloat))) (plusPtr nullPtr (2*sizeOf(0 :: GLfloat))))
+     vertexAttribArray colorLoc $= Enabled
+     drawArrays Triangles 0 6
+     flush
+
+vertexShaderSource = "attribute vec4 a_position;  \
+                     \attribute vec4 a_color;     \
+                     \varying vec4 v_color;       \
+                     \void main() {               \
+                     \  gl_Position = a_position; \
+                     \  v_color = a_color;        \
+                     \}"
+
+fragmentShaderSource = "varying vec4 v_color;     \
+                       \void main() {             \
+                       \  gl_FragColor = v_color; \
+                       \}"
+
+createShader source =
+  do [shader] <- genObjectNames 1
+     shaderSource shader $= [source]
+     compileShader shader
+     return shader
+
+createProgram vertexShader fragmentShader =
+  do [program] <- genObjectNames 1
+     attachedShaders program $= ([vertexShader], [fragmentShader])
+     linkProgram program
+     return program
+
+createBuffer l =
+  do [buffer] <- genObjectNames 1
+     bindBuffer ArrayBuffer $= Just buffer
+     arr <- newListArray (0, length l - 1) l
+     withStorableArray arr $ \ptr ->
+       bufferData ArrayBuffer $= (toEnum (length l*sizeOf(head l)), ptr, StaticDraw)
+     bindBuffer ArrayBuffer $= Nothing
+     return buffer
