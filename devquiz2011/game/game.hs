e8fbf01979d87f0cf830e95776544b4b86f9903d
Satoshi Nakamura <snak@snak.org>
2011-08-29 00:39:21 +0000

    Solved game.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@206 f3cf0176-1a09-0410-9a4c-829831ba871c

--- devquiz2011/game/game.hs
+++ devquiz2011/game/game.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.List (elemIndex, sortBy)
+import Data.List (elemIndex, findIndex, sortBy, subsequences, transpose)
 import Data.Maybe (fromJust)
 import Data.Ord (comparing)
 
@@ -8,14 +8,28 @@ main :: IO ()
 main = interact $ writeOutputs . map solve . readInputs
 
 solve :: [Int] -> Int
-solve input = let m = sortBy (flip $ comparing firstDividable) $ map generate input
-              in solve' (firstDividable $ head m) m
+solve input = let m = map generate input
+                  Just maxIndex = findIndex and $ transpose m
+                  e = estimate input
+                  candidates = filter (\c -> step c < e) $ tail $ subsequences [0..maxIndex]
+              in minimum $ e:(map step $ filter (apply m) candidates)
     where
-      generate = map ((== 0) . flip mod 5) . iterate (flip div 2)
+      step c = last c + length c
+      apply [] _           = True
+      apply _  []          = False
+      apply m  (i:indices) = apply (filter (not . (!! i)) m) indices
+
+estimate :: [Int] -> Int
+estimate input = let m = sortBy (flip $ comparing firstDividable) $ map generate input
+                 in solve' (firstDividable $ head m) m
+    where
       solve' acc []    = acc
       solve' acc (k:m) = solve' (acc + 1) $ remove (firstDividable k) m
       remove v = filter $ not . (!! v)
       firstDividable = fromJust . elemIndex True
+
+generate :: Int -> [Bool]
+generate = map ((== 0) . flip mod 5) . iterate (flip div 2)
 
 readInputs :: String -> [[Int]]
 readInputs = map (map read . words) . pickup . tail . lines
