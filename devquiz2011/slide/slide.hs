efeb2394a3dc152fcb0492812e455fc37226d016
Satoshi Nakamura <snak@snak.org>
2011-09-03 09:32:05 +0000

    Added a bidirectional solver.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@209 f3cf0176-1a09-0410-9a4c-829831ba871c

--- devquiz2011/slide/slide.hs
+++ devquiz2011/slide/slide.hs
@@ -3,16 +3,19 @@
 module Main (main) where
 
 import Control.Monad (forM, liftM)
-import Control.Monad.State (MonadState, evalState, get, modify, put)
+import Control.Monad.State (MonadState, evalState, get, modify, put, runState)
 import Data.Array (Array)
 import Data.Array.IArray (IArray, Ix, (!), (//), assocs, bounds, elems, listArray)
-import Data.List (tails)
+import Data.List (sort, tails)
 import Data.List.Split (sepBy)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Maybe (isJust)
 import Data.Sequence (Seq, ViewL((:<)), (><))
 import qualified Data.Sequence as Seq
 import Data.Set (Set)
 import qualified Data.Set as Set
+import qualified Debug.Trace
 import System.IO (interact)
 
 data Input = Input {
@@ -58,7 +61,7 @@ solveBoard board = solveBoard' board Set.empty
 solveBoard' :: Board -> Set Board -> [Moves]
 solveBoard' board@(Board panels emptyIx) cache
     | Set.member board cache = []
-    | isBoardSorted board = [[]]
+    | isGoalBoard board = [[]]
     | otherwise =
         let nextBoards = [ (m, b) | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D]]
         in concatMap (\(c, b) -> map (c:) $ solveBoard' b (Set.insert board cache)) nextBoards
@@ -71,7 +74,7 @@ solveBoardM' board@(Board panels emptyIx) =
     do cache <- get
        case () of
          _ | Set.member board cache -> return []
-           | isBoardSorted board -> return [[]]
+           | isGoalBoard board -> return [[]]
            | otherwise ->
                do modify (Set.insert board) 
                   let nextBoards = [ (m, b) | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D]]
@@ -91,15 +94,50 @@ solveBoardQ' =
          _ | Set.member board cache ->
                do put (newQueue, cache)
                   solveBoardQ'
-           | isBoardSorted board -> return moves
+           | isGoalBoard board -> return moves
            | otherwise ->
                do let items = [ Item b (m:moves) | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D]]
                   put (newQueue >< Seq.fromList items, Set.insert board cache)
                   solveBoardQ'
 
-isBoardSorted :: Board -> Bool
-isBoardSorted (Board panel _) =
+data Direction = FORWARD
+               | BACKWARD
+  deriving (Eq)
+
+data ItemB = ItemB Board Moves Direction
+
+solveBoardQB :: Board -> Moves
+solveBoardQB board = let (moves, (_, cache)) = runState solveBoardQB' (Seq.fromList [ItemB board [] FORWARD, ItemB (goalBoard board) [] BACKWARD], Map.empty)
+                     in trace (show $ Map.size cache) $ reverse moves
+
+solveBoardQB' :: MonadState (Seq ItemB, Map Board ItemB) m => m Moves
+solveBoardQB' =
+    do (queue, cache) <- get
+       let (ItemB board moves direction) :< newQueue = Seq.viewl queue -- Assume non-empty queue
+       case trace (printBoard board) $ Map.lookup board cache of
+         _ | direction == FORWARD && isGoalBoard board -> return moves
+         Just item@(ItemB _ m d)
+             | d == direction ->
+                 do put (newQueue, cache)
+                    solveBoardQB'
+             | otherwise -> return $ reverse m ++ moves
+         Nothing ->
+               do let items = [ ItemB b (m:moves) direction | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D]]
+                  put (newQueue >< Seq.fromList items, Map.insert board (ItemB board moves direction) cache)
+                  solveBoardQB'
+
+isGoalBoard :: Board -> Bool
+isGoalBoard (Board panel _) =
     all (\[p1, p2] -> p1 < p2) $ map (take 2) $ takeWhile ((>= 2) . length) $ tails $ filter (/= Wall) $ elems panel
+
+goalBoard :: Board -> Board
+goalBoard (Board panels _) =
+    let goalPanels = [ p | p <- sort $ elems panels, p /= Wall ]
+    in Board (listArray (bounds panels) (fill (elems panels) goalPanels)) (snd $ bounds panels)
+    where
+      fill []            _               = []
+      fill (Wall:panels) goalPanels      = Wall:fill panels goalPanels
+      fill (p:panels)    (gp:goalPanels) = gp:fill panels goalPanels
 
 move :: Board -> Move -> Maybe Board
 move (Board panels emptyIx) m
@@ -149,3 +187,17 @@ printMove L = 'L'
 printMove R = 'R'
 printMove U = 'U'
 printMove D = 'D'
+
+printBoard :: Board -> String
+printBoard (Board panels _) =
+    let ((minRow, minColumn), (maxRow, maxColumn)) = bounds panels
+    in show (maxRow - minRow + 1) ++ "," ++ show (maxColumn - minColumn + 1) ++ "," ++ map printPanel (elems panels)
+
+printPanel :: Panel -> Char
+printPanel (Panel c) = c
+printPanel Wall      = '='
+printPanel Empty     = '0'
+
+
+--trace = Debug.Trace.trace
+trace _ a = a
