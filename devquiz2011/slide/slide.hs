31420ccd390788d5341fd03a2a562b317250dfd9
Satoshi Nakamura <snak@snak.org>
2011-09-05 23:04:41 +0000

    Use a distance map to calc distance between boards to improve performance.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@216 f3cf0176-1a09-0410-9a4c-829831ba871c

--- devquiz2011/slide/slide.hs
+++ devquiz2011/slide/slide.hs
@@ -10,6 +10,8 @@ import qualified Data.HashMap.Lazy as HashMap
 import Data.Hashable (Hashable(..))
 import Data.List (sort, sortBy, tails)
 import Data.List.Split (sepBy)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Maybe (fromJust, fromMaybe, isJust)
 import Data.Ord (comparing)
 import Data.PQueue.Min (MinQueue)
@@ -89,14 +91,15 @@ instance Hashable Direction where
     hash FORWARD  = 1
     hash BACKWARD = 2
 
-data Item = Item Board Moves Direction Board Int
+data Item = Item Board Moves Direction Board DistanceMap Int
   deriving Show
 
-makeItem :: Board -> Moves -> Direction -> Board -> Item
-makeItem b m d g = Item b m d g (length m + distance b g)
+makeItem :: Board -> Moves -> Direction -> Board -> DistanceMap -> Int -> Item
+makeItem board moves direction goalBoard distanceMap priority =
+    Item board moves direction goalBoard distanceMap priority
 
 priority :: Item -> Int
-priority (Item _ _ _ _ p) = p
+priority (Item _ _ _ _ _ p) = p
 
 instance Eq Item where
     i1 == i2 = priority i1 == priority i2
@@ -112,18 +115,18 @@ emptyOpenItems = OpenItems PQ.empty HashMap.empty
 getNextOpenItem :: OpenItems -> ClosedItems -> Maybe (Item, OpenItems, ClosedItems)
 getNextOpenItem (OpenItems q t) (ClosedItems m) =
     case PQ.getMin q of
-      Just item@(Item b _ d _ _) | isJust $ HashMap.lookup (b, d) t -> Just $ (item, OpenItems (PQ.deleteMin q) (HashMap.delete (b, d) t), ClosedItems (HashMap.insert b item m))
-                                 | otherwise                        -> getNextOpenItem (OpenItems (PQ.deleteMin q) t) (ClosedItems m)
+      Just item@(Item b _ d _ _ _) | isJust $ HashMap.lookup (b, d) t -> Just $ (item, OpenItems (PQ.deleteMin q) (HashMap.delete (b, d) t), ClosedItems (HashMap.insert b item m))
+                                   | otherwise                        -> getNextOpenItem (OpenItems (PQ.deleteMin q) t) (ClosedItems m)
       Nothing -> Nothing
 
 getOpenItem :: Board -> Direction -> OpenItems -> Maybe Item
 getOpenItem b d (OpenItems _ t) = HashMap.lookup (b, d) t
 
 addOpenItem :: Item -> OpenItems -> OpenItems
-addOpenItem item@(Item b _ d _ _) (OpenItems q t) = OpenItems (PQ.insert item q) (HashMap.insert (b, d) item t)
+addOpenItem item@(Item b _ d _ _ _) (OpenItems q t) = OpenItems (PQ.insert item q) (HashMap.insert (b, d) item t)
 
 removeOpenItem :: Item -> OpenItems -> OpenItems
-removeOpenItem item@(Item b _ d _ _) (OpenItems q t) = OpenItems q (HashMap.delete (b, d) t)
+removeOpenItem item@(Item b _ d _ _ _) (OpenItems q t) = OpenItems q (HashMap.delete (b, d) t)
 
 newtype ClosedItems = ClosedItems (HashMap Board Item) deriving Show
 
@@ -134,44 +137,47 @@ getClosedItem :: Board -> ClosedItems -> Maybe Item
 getClosedItem b (ClosedItems m) = HashMap.lookup b m
 
 removeClosedItem :: Item -> ClosedItems -> ClosedItems
-removeClosedItem (Item b _ _ _ _) (ClosedItems m) = ClosedItems $ HashMap.delete b m
+removeClosedItem (Item b _ _ _ _ _) (ClosedItems m) = ClosedItems $ HashMap.delete b m
 
 getClosedItemsSize :: ClosedItems -> Int
 getClosedItemsSize (ClosedItems m) = HashMap.size m
 
 solveBoard :: Board -> Maybe Moves
 solveBoard board = let goalBoard = getGoalBoard board
-                       initialItems = [makeItem board [] FORWARD goalBoard,
-                                       makeItem goalBoard [] BACKWARD board]
-                       (moves, (_, closedItems)) = runState solveBoard' (foldr addOpenItem emptyOpenItems initialItems, emptyClosedItems)
+                       distanceMap = makeDistanceMap board
+                       goalDistanceMap = makeDistanceMap goalBoard
+                       initialItems = [makeItem board [] FORWARD goalBoard goalDistanceMap (distance goalDistanceMap board),
+                                       makeItem goalBoard [] BACKWARD board distanceMap (distance distanceMap goalBoard)]
+                       (moves, (_, closedItems)) = runState (solveBoard' 0) (foldr addOpenItem emptyOpenItems initialItems, emptyClosedItems)
                    in Debug.Trace.trace (show (fmap length moves) ++ ", " ++ show (getClosedItemsSize closedItems)) $ fmap reverse moves
 
-solveBoard' :: MonadState (OpenItems, ClosedItems) m => m (Maybe Moves)
-solveBoard' =
+solveBoard' :: MonadState (OpenItems, ClosedItems) m => Int -> m (Maybe Moves)
+solveBoard' n =
     do (openItems, closedItems) <- get
        case getNextOpenItem openItems closedItems of
+         _ | n > 300000 -> return Nothing
 --         _ | getClosedItemsSize closedItems > 2000 -> return Nothing
-         Just (item@(Item board moves direction goal priority), nextOpenItems, nextClosedItems) ->
+         Just (item@(Item board moves direction goal distanceMap priority), nextOpenItems, nextClosedItems) ->
              if direction == FORWARD && board == goal then
                  return $ Just moves
              else if direction == BACKWARD && board == goal then
                  return $ Just $ reverse (map reverseMove moves)
              else
                  case (getClosedItem board closedItems, direction) of
-                   (Just (Item _ m BACKWARD _ _), FORWARD) -> return $ Just $ reverse (map reverseMove m) ++ moves
-                   (Just (Item _ m FORWARD _ _), BACKWARD) -> return $ Just $ reverse (map reverseMove moves) ++ m
-                   _ -> do let nextItems = [ makeItem b (m:moves) direction goal | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D] ]
+                   (Just (Item _ m BACKWARD _ _ _), FORWARD) -> return $ Just $ reverse (map reverseMove m) ++ moves
+                   (Just (Item _ m FORWARD _ _ _), BACKWARD) -> return $ Just $ reverse (map reverseMove moves) ++ m
+                   _ -> do let nextItems = [ makeItem b (m:moves) direction goal distanceMap (priority + 1 - panelDistance distanceMap (emptyIx b) (panels board ! emptyIx b) + panelDistance distanceMap (emptyIx board) (panels b ! emptyIx board)) | (m, Just b) <- map (\m -> (m, move board m)) [L, R, U, D] ]
                            put $ foldr insert (nextOpenItems, nextClosedItems) nextItems
-                           solveBoard'
+                           solveBoard' $ n + 1
          Nothing -> return Nothing
     where
-      insert item@(Item b m d g p) (openItems, closedItems)
-             | p > 20 = (openItems, closedItems)
+      insert item@(Item b m d g dm p) (openItems, closedItems)
+             | p > 60 = (openItems, closedItems)
              | otherwise =
           case (getClosedItem b closedItems, getOpenItem b d openItems) of
             (Nothing, Nothing) -> (addOpenItem item openItems, closedItems)
-            (Just closedItem@(Item _ _ cd _ cp), _) | p < cp -> (addOpenItem item openItems, removeClosedItem closedItem closedItems)
-            (_, Just openItem@(Item _ _ od _ op)) | p < op -> (addOpenItem item $ removeOpenItem openItem openItems, closedItems)
+            (Just closedItem@(Item _ _ cd _ _ cp), _) | p < cp -> (addOpenItem item openItems, removeClosedItem closedItem closedItems)
+            (_, Just openItem@(Item _ _ od _ _ op)) | p < op -> (addOpenItem item $ removeOpenItem openItem openItems, closedItems)
             (_, _) -> (openItems, closedItems)
       reverseMove L = R
       reverseMove R = L
@@ -253,12 +259,30 @@ removeFirstRow (Board panel (emptyRow, emptyColumn)) =
 solveFirstColumn :: Board -> (Moves, Board)
 solveFirstColumn board = let (moves, b) = solveFirstRow2 (transposeBoard board) (transposeBoard $ getGoalBoard board)
                          in (map transposeMove moves, transposeBoard b)
--}
+
 distance :: Board -> Board -> Int
 distance b1 b2 = sum $ zipWith dist (indices b1) (indices b2)
     where
       indices (Board panel _ _) = map snd $ sortBy (comparing fst) [ (p, ix) | (ix, p@(Panel _)) <- assocs panel ]
       dist (r1, c1) (r2, c2) = abs (r1 - r2) + abs (c1 - c2)
+-}
+newtype DistanceMap = DistanceMap (Map Panel (Array (Int, Int) Int)) deriving Show
+
+makeDistanceMap :: Board -> DistanceMap
+makeDistanceMap (Board panel _ _) =
+    let ((minRow, minColumn), (maxRow, maxColumn)) = bounds panel
+        indices = [ (r, c) | r <- [minRow .. maxRow], c <- [minColumn .. maxColumn] ]
+        panels = [ (p, ix) | (ix, p@(Panel _)) <- assocs panel ]
+    in DistanceMap $ Map.fromList $ map (\(p, ix) -> (p, listArray (bounds panel) $ map (\i -> dist i ix) indices)) panels
+    where
+      dist (r1, c1) (r2, c2) = abs (r1 - r2) + abs (c1 - c2)
+
+distance :: DistanceMap -> Board -> Int
+distance distanceMap (Board panel _ _) = 
+    sum $ map (uncurry $ panelDistance distanceMap) [ (ix, p) | (ix, p@(Panel _)) <- assocs panel ]
+
+panelDistance :: DistanceMap -> (Int, Int) -> Panel -> Int
+panelDistance  (DistanceMap m) ix panel@(Panel _) = (fromJust $ Map.lookup panel m) ! ix
 
 isGoalBoard :: Board -> Bool
 isGoalBoard (Board panel _ _) =
