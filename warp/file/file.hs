605fbee933fc992a115823444f157cda51cd847e
Satoshi Nakamura <snak@snak.org>
2011-10-27 12:32:51 +0000

    Added a simple warp sample.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@239 f3cf0176-1a09-0410-9a4c-829831ba871c

---
+++ warp/file/file.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Network.HTTP.Types (headerContentType, status200)
+import Network.Wai (Application, Response(..))
+import Network.Wai.Handler.Warp (run)
+
+
+main :: IO ()
+main = run 8000 hello
+
+
+hello :: Application
+hello req = return $ ResponseFile status200 [headerContentType "text/plain"] "file.hs" Nothing
