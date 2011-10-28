42405de19f70d723c6ab0c33c55c4f039cbb3d95
Satoshi Nakamura <snak@snak.org>
2011-10-28 03:39:50 +0000

    Use the common headers.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@246 f3cf0176-1a09-0410-9a4c-829831ba871c

--- warp/path/path.hs
+++ warp/path/path.hs
@@ -9,7 +9,7 @@ import Data.Enumerator.Binary (consume)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend)
 import qualified Data.Text as T
-import Network.HTTP.Types (headerContentType, parseQueryText, status200, status404)
+import Network.HTTP.Types (ResponseHeaders, headerContentType, parseQueryText, status200, status404)
 import Network.Wai (Application, Response(..), pathInfo, responseLBS)
 import Network.Wai.Handler.Warp (run)
 
@@ -30,14 +30,13 @@ dispatch :: DispatchTable -> Application
 dispatch table req =
     case lookup (pathInfo req) table of
       Just app -> app req
-      Nothing -> return $ responseLBS status404 [headerContentType "text/html"] "<h1>Not found</h1>"
+      Nothing -> return $ responseLBS status404 defaultHeaders "<h1>Not found</h1>"
 
 
 root :: Application
 root _ = do
-    let headers = [headerContentType "text/html"]
-        body = fromString "<form method=\"post\" action=\"./hello\"><input type=\"text\" name=\"name\"/></form>"
-    return $ ResponseBuilder status200 headers body
+    let body = fromString "<form method=\"post\" action=\"./hello\"><input type=\"text\" name=\"name\"/></form>"
+    return $ ResponseBuilder status200 defaultHeaders body
     
 
 
@@ -45,9 +44,12 @@ hello :: Application
 hello _ = do
     requestBody <- consume
     let query = parseQueryText $ toStrictByteString requestBody
-        headers = [headerContentType "text/html"]
         body = fromString "Hello, " `mappend`
                fromText (fromMaybe "(Unknown)" $ join $ lookup "name" query) `mappend`
                fromChar '!' `mappend`
                fromString "<br /><a href=\"..\">Back</a>"
-    return $ ResponseBuilder status200 headers body
+    return $ ResponseBuilder status200 defaultHeaders body
+
+
+defaultHeaders :: ResponseHeaders
+defaultHeaders = [headerContentType "text/html; charset=utf-8"]
