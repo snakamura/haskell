aa82c311638ed4234faca67fea5959f954e913b3
Satoshi Nakamura <snak@snak.org>
2011-10-28 01:08:52 +0000

    Added a sample to handle queries.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@242 f3cf0176-1a09-0410-9a4c-829831ba871c

---
+++ warp/query/query.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Blaze.ByteString.Builder.Char.Utf8 (fromChar, fromString, fromText)
+import Control.Monad (join)
+import Data.Maybe (fromMaybe)
+import Data.Monoid (mappend)
+import Network.HTTP.Types (headerContentType, queryToQueryText, status200)
+import Network.Wai (Application, Response(..), queryString)
+import Network.Wai.Handler.Warp (run)
+
+
+main :: IO ()
+main = run 8000 hello
+
+
+hello :: Application
+hello req = do
+  let queryText = queryToQueryText $ queryString req
+      headers = [headerContentType "text/plain"]
+      body = fromString "Hello, " `mappend`
+             fromText (fromMaybe "(Unknown)" $ join $ lookup "name" queryText) `mappend`
+             fromChar '!'
+  return $ ResponseBuilder status200 headers body
