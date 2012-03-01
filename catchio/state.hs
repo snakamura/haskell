17c275ebba97c4aa841137c0ef901db2060f807c
Satoshi Nakamura <snak@snak.org>
2012-03-01 01:08:53 +0000

    Use MonadState and add type notations.
    git-svn-id: https://svn.snak.org/svn/haskell/trunk@260 f3cf0176-1a09-0410-9a4c-829831ba871c

--- catchio/state.hs
+++ catchio/state.hs
@@ -1,35 +1,35 @@
-{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables, FlexibleContexts #-}
 
-import Control.Exception
-import Control.Monad.CatchIO as CatchIO
-import Control.Monad.IO.Class
-import Control.Monad.Trans.State
-import Data.Typeable
+import Control.Exception (Exception)
+import Control.Monad.CatchIO (MonadCatchIO, bracket, catch, throw)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.State.Class (MonadState, get, put)
+import Control.Monad.Trans.State (StateT, runStateT)
+import Data.Typeable (Typeable)
+import Prelude hiding (catch)
 
 data TestException = TestException deriving (Show, Typeable)
 
 instance Exception TestException
 
+test :: MonadCatchIO m => StateT String m String -> m (String, String)
 test f = runStateT go "Init"
     where
-      go = do CatchIO.bracket s e (const f) `CatchIO.catch` c
-              printState "Last"
-              put "Last"
-              return "Last"
-      s = do printState "Start"
-             put "Start"
-      e _ = do printState "End"
-               put "End"
-      c (_ :: SomeException) = do printState "Catch"
-                                  put "Catch"
-                                  return "Catch"
+      go = do bracket (updateState "Start")
+                      (const $ updateState "End")
+                      (const f)
+                  `catch` \(_ :: TestException) -> updateState "Catch"
+              updateState "Last"
 
+updateState :: (MonadIO m, MonadState String m) => String -> m String
+updateState at = do printState at
+                    put at
+                    return at
+
+printState :: (MonadIO m, MonadState String m) => String -> m ()
 printState at = get >>= \state -> liftIO $ print $ "At " ++ at ++ ": " ++ state
 
 test1, test2 :: IO (String, String)
-test1 = test $ do printState "Func"
-                  put "Func"
-                  return "Func"
-test2 = test $ do printState "Func"
-                  put "Func"
-                  liftIO $ throwIO TestException
+test1 = test $ updateState "Func"
+test2 = test $ do updateState "Func"
+                  throw TestException
