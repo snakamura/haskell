3bfb35eee9d9eb429b45563dd9031c88ac38aeec
Satoshi Nakamura <snak@snak.org>
2014-11-13 08:53:28 +0900

    Add an example of using hash with a vector

--- pipes/hash.hs
+++ pipes/hash.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import qualified Control.Foldl as F
 import Control.Monad
 import qualified Crypto.Hash as H
 import qualified Data.ByteString as B
+import qualified Data.Vector as V
 import qualified Pipes as P
 import qualified Pipes.ByteString as PB
 import qualified Pipes.Prelude as P
@@ -19,6 +22,9 @@ hashFile' path = withFile path ReadMode $ hashProducer . PB.fromHandle
 
 hashProducer :: (Monad m, H.HashAlgorithm a) => P.Producer B.ByteString m () -> m (H.Digest a)
 hashProducer = F.purely P.fold hash
+
+hashVector :: V.Vector B.ByteString -> H.Digest H.SHA1
+hashVector = F.fold hash
 
 hash :: H.HashAlgorithm a => F.Fold B.ByteString (H.Digest a)
 hash = F.Fold H.hashUpdate H.hashInit H.hashFinalize
