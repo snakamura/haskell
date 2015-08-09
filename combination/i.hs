23303bdf5b703edd8905fd7618f245e7cfcff237
Satoshi Nakamura <snakamura@infoteria.com>
2015-08-09 12:21:43 +0900

    Remove duplicated results

--- combination/i.hs
+++ combination/i.hs
@@ -1,10 +1,10 @@
 import Data.List
 
 combination :: [Char] -> [([Char], [Char])]
-combination candidates = concatMap (combination' candidates) [0 .. length candidates]
+combination candidates = concatMap (combination' $ sort candidates) [0 .. length candidates]
 
 combination' :: [Char] -> Int -> [([Char], [Char])]
 combination' candidates 0 = [([], candidates)]
-combination' candidates n = concatMap f (combination' candidates (n - 1))
+combination' candidates n = nub $ concatMap f (combination' candidates (n - 1))
   where
-    f (f, s) = map (\c -> (f ++ [c], delete c s)) s
+    f (f, s) = map (\c -> (sort $ f ++ [c], delete c s)) s
