5343e1a4dc20d2101e1b74baffe2373fa8762223
Satoshi Nakamura <snakamura@infoteria.com>
2015-08-09 14:11:58 +0900

    Clean up

--- combination/i.hs
+++ combination/i.hs
@@ -1,17 +1,14 @@
 import Data.List
 
-combination2 :: [Char] -> [[[Char]]]
-combination2 candidates = concatMap (combination2' $ sort candidates) [1 .. length candidates - 1]
-
-combination2' :: [Char] -> Int -> [[[Char]]]
-combination2' candidates 0 = [[[], candidates]]
-combination2' candidates n = nub $ concatMap f (combination2' candidates (n - 1))
-  where
-    f [f, s] = map (\c -> [sort $ f ++ [c], delete c s]) s
-
 combination :: [Char] -> Int -> [[[Char]]]
-combination candidates 2 = combination2 candidates
-combination candidates n = concatMap f $ combination candidates (n - 1)
+combination items 2 = concatMap (split $ sort items) [1 .. length items - 1]
+combination items n = concatMap f $ combination items (n - 1)
   where
     f :: [[Char]] -> [[[Char]]]
-    f l = map ((init l) ++) $ combination2 $ last l
+    f l = map ((init l) ++) $ combination (last l) 2
+
+split :: [Char] -> Int -> [[[Char]]]
+split items 0 = [[[], items]]
+split items n = nub $ concatMap f (split items (n - 1))
+  where
+    f [f, s] = map (\c -> [sort $ f ++ [c], delete c s]) s
