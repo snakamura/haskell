887b18306ffa9d093a1e5298223a2ed68c3fe09f
Satoshi Nakamura <snakamura@infoteria.com>
2015-08-09 13:48:07 +0900

    Add combination3

--- combination/i.hs
+++ combination/i.hs
@@ -1,10 +1,16 @@
 import Data.List
 
-combination :: [Char] -> [([Char], [Char])]
-combination candidates = concatMap (combination' $ sort candidates) [0 .. length candidates]
+combination2 :: [Char] -> [([Char], [Char])]
+combination2 candidates = concatMap (combination2' $ sort candidates) [1 .. length candidates - 1]
 
-combination' :: [Char] -> Int -> [([Char], [Char])]
-combination' candidates 0 = [([], candidates)]
-combination' candidates n = nub $ concatMap f (combination' candidates (n - 1))
+combination2' :: [Char] -> Int -> [([Char], [Char])]
+combination2' candidates 0 = [([], candidates)]
+combination2' candidates n = nub $ concatMap f (combination2' candidates (n - 1))
   where
     f (f, s) = map (\c -> (sort $ f ++ [c], delete c s)) s
+
+combination3 :: [Char] -> [([Char], [Char], [Char])]
+combination3 candidates = concatMap f $ combination2 candidates
+  where
+    f :: ([Char], [Char]) -> [([Char], [Char], [Char])]
+    f (f, s) = map (\(f', s') -> (f, f', s')) $ combination2 s
