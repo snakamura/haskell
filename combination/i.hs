e7d8d5db9a7bf8146a55601fd52dd74bfb63e05d
Satoshi Nakamura <snakamura@infoteria.com>
2015-08-09 15:43:36 +0900

    Clean up

--- combination/i.hs
+++ combination/i.hs
@@ -1,16 +1,20 @@
+import Data.Bifunctor
 import Data.List
 
-combination :: [Char] -> Int -> [[[Char]]]
-combination items 2 = concatMap (map toList . split (sort items)) [1 .. length items - 1]
+combination :: Ord a => [a] -> Int -> [[[a]]]
+combination items 1 = [[items]]
+combination items 2 = concatMap (map toList . pickN (sort items)) [1 .. length items - 1]
   where
-    toList (a, b) = [a, b]
-combination items n = concatMap f $ combination items (n - 1)
+    toList (x, y) = [x, y]
+combination items n = concatMap splitLast $ combination items (n - 1)
   where
-    f :: [[Char]] -> [[[Char]]]
-    f l = map ((init l) ++) $ combination (last l) 2
+    splitLast items = map ((init items) ++) $ combination (last items) 2
 
-split :: [Char] -> Int -> [([Char], [Char])]
-split items 0 = [([], items)]
-split items n = nub $ concatMap f (split items (n - 1))
+pickN :: Ord a => [a] -> Int -> [([a], [a])]
+pickN items 0 = [([], items)]
+pickN items n = nub $ concatMap pickNext $ pickN items (n - 1)
   where
-    f (f, s) = map (\c -> (sort $ f ++ [c], delete c s)) s
+    pickNext (items, restItems) = map (first (sort . (: items))) $ pick restItems
+
+pick :: Eq a => [a] -> [(a, [a])]
+pick items = map (\item -> (item, delete item items)) items
