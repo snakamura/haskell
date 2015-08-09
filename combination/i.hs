31fe256574c4cc61e1108c5e86687eff704bc1f2
Satoshi Nakamura <snakamura@infoteria.com>
2015-08-09 14:40:00 +0900

    Make split use tuple

--- combination/i.hs
+++ combination/i.hs
@@ -1,14 +1,16 @@
 import Data.List
 
 combination :: [Char] -> Int -> [[[Char]]]
-combination items 2 = concatMap (split $ sort items) [1 .. length items - 1]
+combination items 2 = concatMap (map toList . split (sort items)) [1 .. length items - 1]
+  where
+    toList (a, b) = [a, b]
 combination items n = concatMap f $ combination items (n - 1)
   where
     f :: [[Char]] -> [[[Char]]]
     f l = map ((init l) ++) $ combination (last l) 2
 
-split :: [Char] -> Int -> [[[Char]]]
-split items 0 = [[[], items]]
+split :: [Char] -> Int -> [([Char], [Char])]
+split items 0 = [([], items)]
 split items n = nub $ concatMap f (split items (n - 1))
   where
-    f [f, s] = map (\c -> [sort $ f ++ [c], delete c s]) s
+    f (f, s) = map (\c -> (sort $ f ++ [c], delete c s)) s
