0764bbf6a42350303662c7bbbdb8e5383f30a2b4
Satoshi Nakamura <snak@snak.org>
2022-03-27 12:54:39 +0900

    Clean up and add Applicative and Monad instances

--- endo/endofunctor.hs
+++ endo/endofunctor.hs
@@ -19,6 +19,29 @@ instance Monoid' (Endo a) where
     eta () = Endo id
 
 
+class Monoid'' a where
+    type Product'' a
+    type Unit'' a
+    mu' :: Product'' a -> a
+    eta' :: Unit'' a -> a
+
+data EndoComposition a = EndoComposition (Endo a) (Endo a)
+
+composed :: EndoComposition a -> Endo a
+composed (EndoComposition (Endo f) (Endo g)) = Endo $ f . g
+
+data EndoIdentity a = EndoIdentity
+
+identity :: EndoIdentity a -> Endo a
+identity _ = Endo id
+
+instance Monoid'' (Endo a) where
+    type Product'' (Endo a) = EndoComposition a
+    type Unit'' (Endo a) = EndoIdentity a
+    mu' = composed
+    eta' = identity
+
+
 class MonoidF f where
     muF :: Compose f f a -> f a
     etaF :: Identity a -> f a
@@ -30,14 +53,14 @@ instance MonoidF Maybe where
 
 
 class MonoidF' f where
-    type Product' (f :: Type -> Type) :: Type -> Type
-    type Unit' f :: Type -> Type
-    muF' :: Product' f a -> f a
-    etaF' :: Unit' f a -> f a
+    type ProductF' (f :: Type -> Type) :: Type -> Type
+    type UnitF' f :: Type -> Type
+    muF' :: ProductF' f a -> f a
+    etaF' :: UnitF' f a -> f a
 
 instance MonoidF' Maybe where
-    type Product' Maybe = Compose Maybe Maybe
-    type Unit' Maybe = Identity
+    type ProductF' Maybe = Compose Maybe Maybe
+    type UnitF' Maybe = Identity
     muF' (Compose (Just (Just x))) = Just x
     muF' _ = Nothing
     etaF' (Identity x) = Just x
@@ -46,43 +69,43 @@ instance MonoidF' Maybe where
 type f ~> g = forall a. f a -> g a
 
 class MonoidF'' f where
-    type Product'' (f :: Type -> Type) :: Type -> Type
-    type Unit'' f :: Type -> Type
-    muF'' :: Product'' f ~> f
-    etaF'' :: Unit'' f ~> f
+    type ProductF'' (f :: Type -> Type) :: Type -> Type
+    type UnitF'' f :: Type -> Type
+    muF'' :: ProductF'' f ~> f
+    etaF'' :: UnitF'' f ~> f
 
 instance MonoidF'' Maybe where
-    type Product'' Maybe = Compose Maybe Maybe
-    type Unit'' Maybe = Identity
+    type ProductF'' Maybe = Compose Maybe Maybe
+    type UnitF'' Maybe = Identity
     muF'' (Compose (Just (Just x))) = Just x
     muF'' _ = Nothing
     etaF'' (Identity x) = Just x
 
 instance MonoidF'' ((->) r) where
-    type Product'' ((->) r) = Compose ((->) r) ((->) r)
-    type Unit'' ((->) r) = Identity
+    type ProductF'' ((->) r) = Compose ((->) r) ((->) r)
+    type UnitF'' ((->) r) = Identity
     muF'' (Compose f) e = f e e
     etaF'' (Identity x) = const x
 
 newtype R r a = R (r -> a)
 
 instance MonoidF'' (R r) where
-    type Product'' (R r) = Compose (R r) (R r)
-    type Unit'' (R r) = Identity
+    type ProductF'' (R r) = Compose (R r) (R r)
+    type UnitF'' (R r) = Identity
     muF'' (Compose (R f)) = R $ \e -> let R g = f e in g e
     etaF'' (Identity x) = R $ const x
 
 instance Monoid w => MonoidF'' ((,) w) where
-    type Product'' ((,) w) = Compose ((,) w) ((,) w)
-    type Unit'' ((,) w) = Identity
+    type ProductF'' ((,) w) = Compose ((,) w) ((,) w)
+    type UnitF'' ((,) w) = Identity
     muF'' (Compose (w2, (w1, x))) = (w1 <> w2, x)
     etaF'' (Identity x) = (mempty, x)
 
 newtype W w a = W (a, w)
 
 instance Monoid w => MonoidF'' (W w) where
-    type Product'' (W w) = Compose (W w) (W w)
-    type Unit'' (W w) = Identity
+    type ProductF'' (W w) = Compose (W w) (W w)
+    type UnitF'' (W w) = Identity
     muF'' (Compose (W (W (x, w1), w2))) = W (x, w1 <> w2)
     etaF'' (Identity x) = W (x, mempty)
 
@@ -97,6 +120,18 @@ instance MonoidF f => Monad' f where
     pure' = etaF . Identity
 -}
 
-instance (MonoidF'' f, Product'' f ~ Compose f f, Unit'' f ~ Identity) => Monad' f where
+instance (MonoidF'' f, ProductF'' f ~ Compose f f, UnitF'' f ~ Identity) => Monad' f where
     join' = muF'' . Compose
     pure' = etaF'' . Identity
+
+instance (Functor f, MonoidF'' f, ProductF'' f ~ Compose f f, UnitF'' f ~ Identity) => Applicative f where
+--    (<*>) f m = f >>= \f' -> m >>= \m' -> pure (f' m')
+--    (<*>) f m = (>>=) f (\f' -> m >>= \m' -> pure (f' m'))
+--    (<*>) f m = muF'' $ Compose $ fmap (\f' -> m >>= \m' -> pure (f' m')) f
+--    (<*>) f m = muF'' $ Compose $ fmap (\f' -> (>>=) m (\m' -> pure (f' m'))) f
+--    (<*>) f m = muF'' $ Compose $ fmap (\f' -> muF'' $ Compose $ fmap (\m' -> pure (f' m')) m) f
+    (<*>) f m = muF'' $ Compose $ fmap (\f' -> muF'' $ Compose $ fmap (pure . f') m) f
+    pure = etaF'' . Identity
+
+instance (Functor f, MonoidF'' f, ProductF'' f ~ Compose f f, UnitF'' f ~ Identity) => Monad f where
+    (>>=) m f = muF'' $ Compose $ fmap f m
