d18103f1e2871374237f96fe688914cfb7ae5cfe
Satoshi Nakamura <snak@snak.org>
2022-05-19 08:43:28 +0900

    Add type3

---
+++ related-type/type3/Main.hs
@@ -0,0 +1,19 @@
+module Main (main) where
+
+import Data.Maybe(catMaybes)
+import Item
+    ( SomeItem(SomeItem)
+    , SCat(..)
+    , SSubCat(..)
+    , buildItem
+    )
+
+main :: IO ()
+main = do
+    let item1 = buildItem SCat1 SSubCat2
+        item2 = buildItem SCat2 SSubCat3
+        item3 = buildItem SCat2 SSubCat1
+    mapM_ print $ catMaybes [ SomeItem <$> item1
+                            , SomeItem <$> item2
+                            , SomeItem <$> item3
+                            ]
