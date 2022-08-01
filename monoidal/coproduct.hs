0e446d1e2ded96bf4c9ef20e60b5e8a3b174483d
Satoshi Nakamura <snak@snak.org>
2022-08-01 09:11:41 +0900

    Update monoidal functor examples

--- monoidal/coproduct.hs
+++ monoidal/coproduct.hs
@@ -1,21 +1,14 @@
-{-# LANGUAGE DeriveFunctor,
-             FlexibleInstances,
+{-# LANGUAGE FlexibleInstances,
              InstanceSigs,
-             NoImplicitPrelude,
              ScopedTypeVariables,
              UndecidableInstances
 #-}
 
-import Data.Either (Either(..))
-import Data.Functor (Functor(..))
 import Data.Void (Void)
-import Text.Show (Show)
 
 class Functor f => Monoidal f where
     op :: Either (f a) (f b) -> f (Either a b)
     unit :: f Void
-
-data Maybe a = Just a | Nothing deriving (Show, Functor)
 
 instance Monoidal Maybe where
     op :: Either (Maybe a) (Maybe b) -> Maybe (Either a b)
--- monoidal/product.hs
+++ monoidal/product.hs
@@ -1,27 +1,17 @@
-{-# LANGUAGE DeriveFunctor,
-             FlexibleInstances,
+{-# LANGUAGE FlexibleInstances,
              InstanceSigs,
-             NoImplicitPrelude,
              ScopedTypeVariables,
              UndecidableInstances
 #-}
-
-import Control.Applicative (Applicative(..))
-import Data.Function (const)
-import Data.Functor (Functor(..))
-import Text.Show (Show)
 
 class Functor f => Monoidal f where
     op :: (f a, f b) -> f (a, b)
     unit :: f ()
 
-data Maybe a = Just a | Nothing deriving (Show, Functor)
-
 instance Monoidal Maybe where
     op :: (Maybe a, Maybe b) -> Maybe (a, b)
     op (Just a, Just b) = Just (a, b)
-    op (Just _, Nothing) = Nothing
-    op (Nothing, _) = Nothing
+    op _ = Nothing
 
     unit :: Maybe ()
     unit = Just ()
