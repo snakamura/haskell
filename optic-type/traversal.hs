9bd80cf06950458d18c0f0a33d9874d8de9dfd82
Satoshi Nakamura <snak@snak.org>
2023-09-19 15:33:17 +0900

    Update traversal

--- optic-type/traversal.hs
+++ optic-type/traversal.hs
@@ -1,6 +1,31 @@
 module Traversal where
 
+import Data.Foldable
+import Data.Functor.Contravariant
+
+type Fold s a = forall f. (Functor f, Contravariant f, Applicative f) => (a -> f a) -> (s -> f s)
+
+folding' :: Foldable g => Fold (g a) a
+folding' = \afb -> \ga ->
+  let fb = traverse_ afb ga
+   in fb $< ()
+
+
 type Traversal s t a b = forall f. Applicative f => (a -> f b) -> (s -> f t)
+
+traversing :: Traversable g => Traversal (g a) (g b) a b
+traversing = \afb -> \ga ->
+  let fgb = traverse afb ga
+   in fgb
+
+traversing' :: Traversable g => Traversal (g a) (g b) a b
+traversing' = traverse
+
+setting :: Traversable g => Traversal (g a) (g b) a b
+setting = \afb -> \ga ->
+  let fgb = traverse afb ga
+   in fgb
+
 
 both :: Traversal (a, a) (b, b) a b
 both = \afb -> \(a1, a2) ->
