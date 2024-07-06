c7e77ce785da111c96c4ad3acc1e597c52876e5a
Satoshi Nakamura <snak@snak.org>
2024-07-06 16:24:59 +0900

    fixup! Add extensible

--- fields/Extensible.hs
+++ fields/Extensible.hs
@@ -1,23 +1,25 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE DataKinds, OverloadedLabels #-}
 
 module Extensible where
 
 import Control.Lens
 import Data.Extensible
 
-type Person = Record '["name" >: Name, "age" >: Int]
+type Person = Record '[ "name" >: Name
+                      , "age" >: Int
+                      ]
 
-type Name = Record '["first" >: String, "last" >: String]
+type Name = Record '[ "first" >: String
+                    , "last" >: String
+                    ]
 
 person :: Person
-person =
-     #name @= (    #first @= "Tiger"
-                <: #last @= "Scott"
-                <: nil
-              )
-  <: #age @= 10
-  <: nil
+person = #name @= (    #first @= "Tiger"
+                    <: #last @= "Scott"
+                    <: nil
+                  )
+      <: #age @= 10
+      <: nil
 
 f :: String
 f = person ^. xlb #name . xlb #first
