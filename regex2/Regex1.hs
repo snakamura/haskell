87a13cd9798eb97c870cb1a465b2f59635daaa27
Satoshi Nakamura <snak@snak.org>
2025-01-27 09:52:30 +0900

    Add another version of Regex1

--- regex2/Regex1.hs
+++ regex2/Regex1.hs
@@ -34,3 +34,42 @@ match' (Char rc) (c : cs)
 match' (Char _) "" = []
 match' (Seq r1 r2) s = match' r1 s >>= match' r2
 match' (Alt r1 r2) s = match' r1 s <> match' r2 s
+
+
+{-
+module Regex14 where
+
+data Regex
+  = Never
+  | Empty
+  | Char Char
+  | Seq Regex Regex
+  | Alt Regex Regex
+  | Many Regex
+  deriving (Show)
+
+regex0, regex1, regex2, regex3, regex4, regex5, regex6, regex7, regex8 :: Regex
+regex0 = Never
+regex1 = Empty -- //
+regex2 = Char 'a' -- /a/
+regex3 = Char 'a' `Seq` Char 'b' -- /ab/
+regex4 = Char 'a' `Alt` Char 'b' -- /a|b/
+regex5 = Empty `Alt` Char 'a' -- /|a/
+regex6 = Many (Char 'a') -- /a*/
+regex7 = (Char 'a' `Seq` Char 'b') `Alt` (Many (Char 'c' `Seq` Char 'd') `Seq` Char 'e') -- /ab|(cd)*e/
+regex8 = Many (Char 'a') `Seq` Char 'a' -- /a*a/
+
+match :: Regex -> String -> Bool
+match r s = elem "" $ match' r s
+
+match' :: Regex -> String -> [String]
+match' Never _ = []
+match' Empty s = [s]
+match' (Char rc) (c : cs)
+  | rc == c = [cs]
+  | otherwise = []
+match' (Char _) "" = []
+match' (Seq r1 r2) s = match' r1 s >>= match' r2
+match' (Alt r1 r2) s = match' r1 s <> match' r2 s
+match' many@(Many r) s = (match' r s >>= match' many) <> [s]
+-}
