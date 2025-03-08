8ff764ee76eadcb8556ba991c193f33fa54966ee
Satoshi Nakamura <snak@snak.org>
2025-03-08 22:20:16 +0900

    Add adjunctions for SomeF and AnyF

--- exists_const_forall/Any.hs
+++ exists_const_forall/Any.hs
@@ -53,3 +53,25 @@ anySVShow (MkAnySV sv) =
 
 anySV :: AnySV
 anySV = MkAnySV undefined
+
+forward :: (forall x. a -> f x) -> (a -> AnyF f)
+forward g = \a -> MkAnyF (g a)
+
+backward :: (a -> AnyF f) -> (forall x. a -> f x)
+backward h = \a -> let MkAnyF fx = h a in fx
+
+newtype Const a x = MkConst a
+
+forward' :: (forall x. Const a x -> f x) -> (a -> AnyF f)
+forward' g = \a -> MkAnyF (g (MkConst a))
+
+backward' :: (a -> AnyF f) -> (forall x. Const a x -> f x)
+backward' h = \(MkConst a) -> let MkAnyF fx = h a in fx
+
+type f ~> g = forall x. f x -> g x
+
+leftAdjunct :: (Const a ~> f) -> (a -> AnyF f)
+leftAdjunct g = \a -> MkAnyF (g (MkConst a))
+
+rightAdjunct :: (a -> AnyF f) -> (Const a ~> f)
+rightAdjunct h = \(MkConst a) -> let MkAnyF fx = h a in fx
--- exists_const_forall/Some.hs
+++ exists_const_forall/Some.hs
@@ -52,3 +52,25 @@ someSVShow (MkSomeSV sv) =
 someSV1, someSV2 :: SomeSV
 someSV1 = MkSomeSV (MkSV1 1)
 someSV2 = MkSomeSV (MkSV2 "x")
+
+forward :: (SomeF f -> a) -> (forall x. f x -> a)
+forward g = \fx -> g (MkSomeF fx)
+
+backward :: (forall x. f x -> a) -> (SomeF f -> a)
+backward h = \(MkSomeF fx) -> h fx
+
+newtype Const a x = MkConst a
+
+forward' :: (SomeF f -> a) -> (forall x. f x -> Const a x)
+forward' g = \fx -> MkConst (g (MkSomeF fx))
+
+backward' :: (forall x. f x -> Const a x) -> (SomeF f -> a)
+backward' h = \(MkSomeF fx) -> let MkConst a = h fx in a
+
+type f ~> g = forall x. f x -> g x
+
+leftAdjunct :: (SomeF f -> a) -> (f ~> Const a)
+leftAdjunct g = \fx -> MkConst (g (MkSomeF fx))
+
+rightAdjunct :: (f  ~> Const a) -> (SomeF f -> a)
+rightAdjunct h = \(MkSomeF fx) -> let MkConst a = h fx in a
