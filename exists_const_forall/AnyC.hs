a24814213feaa5617b9d85891d596de21fa7a045
Satoshi Nakamura <snak@snak.org>
2025-03-12 08:28:41 +0900

    Add forward and backward for SomeC and AnyC

--- exists_const_forall/AnyC.hs
+++ exists_const_forall/AnyC.hs
@@ -5,14 +5,26 @@ import Data.Kind
 type AnyC :: (Type -> Constraint) -> Type
 newtype AnyC c = MkAnyC (forall a. (c a) => a)
 
-anyCNum :: AnyC Num -> Int
-anyCNum (MkAnyC a) = a
-
 anyCNumValue :: AnyC Num
 anyCNumValue = MkAnyC 1
 
-anyCRead :: AnyC Read -> String
-anyCRead (MkAnyC a) = let n = a :: Int in show n
+fromAnyCNum :: AnyC Num -> Int
+fromAnyCNum (MkAnyC a) = a
+
+toAnyCNum :: Int -> AnyC Num
+toAnyCNum n = MkAnyC (fromInteger (toInteger n))
 
 anyCReadValue :: AnyC Read
 anyCReadValue = MkAnyC (read "1")
+
+fromAnyCShow :: AnyC Show -> String
+fromAnyCShow (MkAnyC a) = let n = a :: Int in show n
+
+toAnyCRead :: String -> AnyC Read
+toAnyCRead s = MkAnyC (read s)
+
+forward :: (forall x. c x => a -> x) -> (a -> AnyC c)
+forward g = \a -> MkAnyC (g a)
+
+backward :: (a -> AnyC c) -> (forall x. c x => a -> x)
+backward h = \a -> let MkAnyC x = h a in x
--- exists_const_forall/SomeC.hs
+++ exists_const_forall/SomeC.hs
@@ -6,8 +6,17 @@ type SomeC :: (Type -> Constraint) -> Type
 data SomeC c where
   MkSomeC :: (c a) => a -> SomeC c
 
-someCShow :: SomeC Show -> String
-someCShow (MkSomeC a) = show a
-
 someCShowValue :: SomeC Show
 someCShowValue = MkSomeC (1 :: Int)
+
+fromSomeCShow :: SomeC Show -> String
+fromSomeCShow (MkSomeC a) = show a
+
+toSomeCRead :: String -> SomeC Read
+toSomeCRead s = MkSomeC (read @Int s)
+
+forward :: (SomeC c -> a) -> (forall x. c x => x -> a)
+forward g = \x -> g (MkSomeC x)
+
+backward :: (forall x. c x => x -> a) -> (SomeC c -> a)
+backward h = \(MkSomeC x) -> h x
