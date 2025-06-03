647f1a1f3286fcb4b7554b153c719b68d3dda5e5
Satoshi Nakamura <snak@snak.org>
2025-06-04 08:01:47 +0900

    Clean up FreeMonoid

--- f_algebra/FreeMonoid.hs
+++ f_algebra/FreeMonoid.hs
@@ -4,6 +4,11 @@ data FreeMonoid a r = Append r r
                     | Empty
                     | Lift a
 
+instance Functor (FreeMonoid a) where
+    fmap f (Append n m) = Append (f n) (f m)
+    fmap _ Empty = Empty
+    fmap _ (Lift n) = Lift n
+
 sumIntAlg :: FreeMonoid Int Int -> Int
 sumIntAlg (Append m n) = m + n
 sumIntAlg Empty = 0
@@ -42,23 +47,31 @@ out (In f) = f
 initialAlg :: FreeMonoid a (Fix (FreeMonoid a)) -> Fix (FreeMonoid a)
 initialAlg = In
 
-m0, m1, m2 :: FreeMonoid Int (Fix (FreeMonoid Int))
-m0 = Empty
-m1 = (Append (In Empty) (In Empty))
-m2 = (Append (In (Lift 1)) (In (Lift 2)))
+m0, m1, m2, m3 :: Fix (FreeMonoid Int)
+m0 = In Empty
+m1 = In (Lift 1)
+m2 = In (Append (In (Append (In (Lift 1)) (In (Lift 2)))) (In Empty))
+m3 = In (Append (In (Append (In (Lift 1)) (In (Append (In (Lift 2)) (In (Lift 3)))))) (In Empty))
 
-instance Functor (FreeMonoid a) where
-    fmap f (Append n m) = Append (f n) (f m)
-    fmap _ Empty = Empty
-    fmap _ (Lift n) = Lift n
+homInitialToSumInt :: Fix (FreeMonoid Int) -> Int
+homInitialToSumInt = sumIntAlg . fmap homInitialToSumInt . out
+
+i0, i1, i2, i3 :: Int
+i0 = homInitialToSumInt m0
+i1 = homInitialToSumInt m1
+i2 = homInitialToSumInt m2
+i3 = homInitialToSumInt m3
 
 homInitialToInt :: (FreeMonoid Int Int -> Int) -> Fix (FreeMonoid Int) -> Int
 homInitialToInt alg = alg . fmap (homInitialToInt alg) . out
 
-v0', v1', v2' :: Int
-v0' = homInitialToInt sumIntAlg (In m0)
-v1' = homInitialToInt sumIntAlg (In m1)
-v2' = homInitialToInt sumIntAlg (In m2)
-
-hom :: Functor f => (f a -> a) -> Fix f -> a
+hom :: (FreeMonoid a a -> a) -> Fix (FreeMonoid a) -> a
 hom alg = alg . fmap (hom alg) . out
+
+cata :: Functor f => (f a -> a) -> Fix f -> a
+cata alg = alg . fmap (cata alg) . out
+
+i0', i1', i2' :: Int
+i0' = cata sumIntAlg m0
+i1' = cata sumIntAlg m1
+i2' = cata sumIntAlg m2
