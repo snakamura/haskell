0a162fc0647ae089bd5ffb368ecc75d6c573abdc
Satoshi Nakamura <snak@snak.org>
2025-08-27 09:38:24 +0900

    Add leftAdjunct and rightAdjunct

--- pair_function/Curry.hs
+++ pair_function/Curry.hs
@@ -8,3 +8,9 @@ g = uncurry f
 
 h :: a -> (b -> c)
 h = f
+
+leftAdjunct :: ((,) e a -> b) -> (a -> (->) e b)
+leftAdjunct ea2b a e = ea2b (e, a)
+
+rightAdjunct :: (a -> (->) e b) -> ((,) e a -> b)
+rightAdjunct a2e2b (e, a) = a2e2b a e
