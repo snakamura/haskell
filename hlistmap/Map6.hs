b5625d275c750a92f2b8a9da73827f8ac1891f84
Satoshi Nakamura <snak@snak.org>
2025-10-18 16:32:51 +0900

    Add Map6

---
+++ hlistmap/Map6.hs
@@ -0,0 +1,51 @@
+module Map6 where
+
+import Data.Kind
+import GHC.Base hiding (map)
+import GHC.TypeLits
+import Data.Proxy
+import Prelude hiding (map)
+
+type HList :: [Type] -> Type
+data HList xs where
+    HNil :: HList '[]
+    HCons :: x -> HList xs -> HList (x ': xs)
+
+infixr `HCons`
+
+type Literal :: Symbol -> Type
+newtype Literal n = Literal String
+
+makeLiteral :: forall (n :: Symbol) -> KnownSymbol n => Literal n
+makeLiteral n = Literal (symbolVal (Proxy @n))
+
+makeLiteral' :: KnownSymbol n => Literal n
+makeLiteral' @n = Literal (symbolVal (Proxy @n))
+
+type Object :: Type -> Type
+newtype Object n = Object { name :: n }
+
+objects :: HList [Object (Literal "a"), Object (Literal "b"), Object (Literal "c")]
+objects = Object { name = makeLiteral "a" } `HCons`
+          Object { name = makeLiteral "b" } `HCons`
+          Object { name = makeLiteral "c" } `HCons`
+          HNil
+
+type MapType :: [Type] -> [Type]
+type family MapType xs where
+    MapType '[] = '[]
+    MapType (Object n ': xs) = n ': MapType xs
+
+data IsObject n where
+    IsObject :: IsObject (Object n)
+
+data AreObjects ns where
+    AreObjectsNil :: AreObjects '[]
+    AreObjectsCons :: IsObject n -> AreObjects ns -> AreObjects (n ': ns)
+
+map :: AreObjects xs -> (forall n. Object n -> n) -> HList xs -> HList (MapType xs)
+map AreObjectsNil _ HNil = HNil
+map (AreObjectsCons IsObject o) f (HCons x xs) = HCons (f x) (map o f xs)
+
+mappedNames :: HList [Literal "a", Literal "b", Literal "c"]
+mappedNames = map (AreObjectsCons IsObject (AreObjectsCons IsObject (AreObjectsCons IsObject AreObjectsNil))) name objects
