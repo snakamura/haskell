d3c69bc06be27089a4d42f9a178fca2b59537ef7
Satoshi Nakamura <snak@snak.org>
2025-11-12 07:44:06 +0900

    Add hoistCostar

--- strength/Profunctor/DinaturalTransformation.hs
+++ strength/Profunctor/DinaturalTransformation.hs
@@ -1,5 +1,6 @@
 module Profunctor.DinaturalTransformation where
 
+import Profunctor.Costar
 import Profunctor.Star
 
 -- lmap f . alphaB . rmap f = rmap f . alphaA . lmap f
@@ -29,10 +30,25 @@ hoistStar fx2gx (Star a2fa) = Star (fx2gx . a2fa)
 -- f :: a -> b
 --
 -- rmap f p = fmap f . p :: b -> f b
--- (hoistStar . rmap f) p = fx2gx . fmap f . p :: b -> g b
--- (lmap f . hoistStar . rmap f) p = fx2gx . fmap f . p . f :: a -> g b
+-- (hoistStar fx2gx . rmap f) p = fx2gx . fmap f . p :: b -> g b
+-- (lmap f . hoistStar fx2gx . rmap f) p = fx2gx . fmap f . p . f :: a -> g b
 --
 -- lmap f p = p . f :: a -> f a
--- (hoistStar . lmap f) p = fx2gx . p . f :: a -> g a
--- (rmap f . hoistStar . lmap f) p = fmap f . fx2gx . p . f :: a -> g b
---                                 = fx2gx . fmap f . p . f :: a -> g b -- fmap f . fx2gx = fx2gx . fmap f
+-- (hoistStar fx2gx . lmap f) p = fx2gx . p . f :: a -> g a
+-- (rmap f . hoistStar fx2gx . lmap f) p = fmap f . fx2gx . p . f :: a -> g b
+      --                                 = fx2gx . fmap f . p . f :: a -> g b -- fmap f . fx2gx = fx2gx . fmap f
+
+hoistCostar :: forall f g a. (forall x. g x -> f x) -> Costar f a a -> Costar g a a
+hoistCostar gx2fx (Costar fa2a) = Costar (fa2a . gx2fx)
+
+-- p :: Costar f b a -- f b -> a
+-- f :: a -> b
+--
+-- rmap f p = f . p :: f b -> b
+-- (hoistCostar gx2fx . rmap f) p = f . p . gx2fx :: g b -> b
+-- (lmap f . hoistCostar gx2fx . rmap f) p = f . p . gx2fx . fmap f :: g a -> b
+--
+-- lmap f p = p . fmap f :: f a -> a
+-- (hoistCostar gx2fx . lmap f) p = p . fmap f . gx2fx :: g a -> a
+-- (rmap f . hoistCostar gx2fx . lmap f) p = f . p . fmap f . gx2fx :: g a -> b
+--                                         = f . p . gx2fx . fmap f :: g a -> b -- fmap f . gx2fx = gx2fx . fmap f
