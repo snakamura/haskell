44317f04a5c74d66378097834b80d1ee277681c5
Satoshi Nakamura <snak@snak.org>
2026-01-03 09:14:55 +0900

    Update natural transformations

--- strength/Functor/NaturalTransformation.hs
+++ strength/Functor/NaturalTransformation.hs
@@ -5,6 +5,38 @@ import Data.List
 
 type f ~> g = forall x. f x -> g x
 
+{-
+maybeToList :: Maybe ~> []
+maybeToList (Just x) = [x]
+maybeToList Nothing = []
+-}
+
+maybeToListL,
+  maybeToListL',
+  maybeToListL'',
+  maybeToListL''' ::
+    (a -> b) -> Maybe a -> [b]
+maybeToListL a2b = fmap a2b . maybeToList
+maybeToListL' a2b Nothing = fmap a2b [] $ maybeToList Nothing
+maybeToListL' a2b (Just a) = fmap a2b [a] $ maybeToList (Just a)
+maybeToListL'' a2b Nothing = fmap a2b []
+maybeToListL'' a2b (Just a) = fmap a2b [a]
+maybeToListL''' a2b Nothing = []
+maybeToListL''' a2b (Just a) = [a2b a]
+
+maybeToListR,
+  maybeToListR',
+  maybeToListR'',
+  maybeToListR''' ::
+    (a -> b) -> Maybe a -> [b]
+maybeToListR a2b = maybeToList . fmap a2b
+maybeToListR' a2b Nothing = maybeToList $ fmap a2b Nothing
+maybeToListR' a2b (Just a) = maybeToList $ fmap a2b (Just a)
+maybeToListR'' a2b Nothing = maybeToList Nothing
+maybeToListR'' a2b (Just a) = maybeToList (Just (a2b a))
+maybeToListR''' a2b Nothing = []
+maybeToListR''' a2b (Just a) = [a2b a]
+
 -- Three functors from Hask to Hask
 -- (,) Bool, Maybe, List and
 
