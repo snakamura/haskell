20a94da9097d7d73bdb5debf2e7827f36e5370d0
Satoshi Nakamura <snak@snak.org>
2026-01-15 07:57:21 +0900

    Add Hom and HomOp

--- ends/Functor.hs
+++ ends/Functor.hs
@@ -1,7 +1,9 @@
 module Functor where
 
-import Data.Profunctor
 import Data.Functor.Contravariant
+import Data.Functor.Const
+import Data.Kind
+import Data.Profunctor
 
 newtype WrappedFunctor f a b = WrappedFunctor (f b)
 
@@ -14,3 +16,20 @@ newtype WrappedContravariant f a b = WrappedContravariant (f a)
 instance (Contravariant f) => Profunctor (WrappedContravariant f) where
   dimap :: (s -> a) -> (b -> t) -> (WrappedContravariant f a b -> WrappedContravariant f s t)
   dimap s2a _ (WrappedContravariant fa) = WrappedContravariant (contramap s2a fa)
+
+type Hom :: (Type -> Type) -> (Type -> Type) -> Type -> Type -> Type
+newtype Hom f g a b = Hom (f a -> g b)
+
+instance (Functor f, Functor g) => Profunctor (Hom f g) where
+  dimap :: (s -> a) -> (b -> t) -> (Hom f g a b -> Hom f g s t)
+  dimap s2a b2t (Hom h) = Hom (fmap b2t . h . fmap s2a)
+
+instance (Functor g) => Functor (Hom (Const c) g a) where
+  fmap :: (b -> t) -> (Hom (Const c) g a b -> Hom (Const c) g a t)
+  fmap = rmap
+
+newtype HomOp f g a b = HomOp (Hom f g b a)
+
+instance (Functor f) => Contravariant (HomOp f (Const c) b) where
+  contramap :: (s -> a) -> (HomOp f (Const c) b a -> HomOp f (Const c) b s)
+  contramap s2a (HomOp hom)= HomOp (lmap s2a hom)
