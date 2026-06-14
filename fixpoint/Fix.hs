ff5d1c10c185a62924f75cb75819026ff6bd43b6
Satoshi Nakamura <snak@snak.org>
2026-06-14 17:59:54 +0900

    Add para and apo to Fix

--- fixpoint/Fix.hs
+++ fixpoint/Fix.hs
@@ -16,3 +16,16 @@ embed = Fix
 
 project :: Fix f -> f (Fix f)
 project (Fix ffixf) = ffixf
+
+para :: (Functor f) => (f (Fix f, a) -> a) -> Fix f -> a
+para @f @a phi = snd . cata alg
+  where
+    alg :: f (Fix f, a) -> (Fix f, a)
+    alg x = (embed (fst <$> x), phi x)
+
+apo :: (Functor f) => (a -> f (Either (Fix f) a)) -> a -> Fix f
+apo @f @a psi = ana coalg . Right
+  where
+    coalg :: Either (Fix f) a -> f (Either (Fix f) a)
+    coalg (Left nu) = Left <$> project nu
+    coalg (Right a) = psi a
